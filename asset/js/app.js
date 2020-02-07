c2c01c5d2fe862b60648f088ec430485fcc11fef
Rado Nirina <toarymanana@gmail.com>
2020-02-07 10:53:09 +0300

    modification app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,4 +1,10 @@
 $(function(){
-
+     
+     var oppose=window.innerHeight-$('.body_ennemies').offset().top;
+     var adjacent = $('.body_defender').offset().left;
+     
+     var hypothenus = Math.sqrt((oppose*oppose)+(adjacent*adjacent));
+     
+      
 
 });
