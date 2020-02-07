2d1bc00ff2c29893dea00efe63b76f23095c8ad8
Rado Nirina <toarymanana@gmail.com>
2020-02-07 13:57:00 +0300

    correction cannon ennemies

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,10 +1,25 @@
 $(function(){
      
-     var oppose=window.innerHeight-$('.body_ennemies').offset().top;
-     var adjacent = $('.body_defender').offset().left;
-     
-     var hypothenus = Math.sqrt((oppose*oppose)+(adjacent*adjacent));
-     
+
+     var  top = 0;
+     /* setInterval(()=>{
       
+    if(window.innerHeight>top){
+     var oppose=window.innerHeight-$('.body_ennemies').offset().top;
+     var adjacent1 = $('.body_defender').offset().left;
+     var adjacent2 = $('.body_ennemies').offset().left;
+     var adjacent = -adjacent1 +adjacent2;
+    /*var hypothenus = Math.sqrt((oppose*oppose)+(adjacent*adjacent));
+     var cosinus = adjacent/hypothenus; */
+    /* var angleRadiant = Math.atan(adjacent/oppose);
+     var angleDegre = angleRadiant*180/3.1415926535;
+     
+     $('.body_ennemies').css('top',top+'px');
+     //console.log(top);
+     top=top+20;
+
+     $('.fire_enimis').css('transform','rotate('+angleDegre+'deg)');
+     }
+     },200);   */
 
 });
